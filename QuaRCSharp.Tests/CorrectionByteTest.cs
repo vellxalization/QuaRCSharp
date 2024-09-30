35f65fddfc3f0310d742614126409160b9316a5a
vellxalization <abcd1234abcd12346738t68374@gmail.com>
2024-09-30 19:00:44 +0300

    more tests

---
+++ QuaRCSharp.Tests/CorrectionByteTest.cs
@@ -0,0 +1,20 @@
+﻿using QuaRCSharp.Data;
+using QuaRCSharp.QRCodes;
+
+namespace QuaRCSharp.Tests;
+
+public class CorrectionByteTest
+{
+    [Theory]
+    [InlineData(new byte[] {}, new byte[] {0, 0, 0, 0, 0, 0, 0})]
+    [InlineData(new byte[] { 0 }, new byte[] {0, 0, 0, 0, 0, 0, 0})]
+    [InlineData(new byte[] { 1, 2, 3, 4, 5, 6 }, new byte[] { 16, 29, 73, 187, 244, 214, 218 })]
+    [InlineData(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new byte[] { 186, 221, 200, 172, 182, 137, 52 })]
+    public void GenerateCorrectionBytes(byte[] message, params byte[][] expectedBytes)
+    {
+        var generator = new CorrectionByteGenerator();
+        var correctionBytes = generator.CreateErrorCorrectionBytesForByteChain([message], new QRCodeVersion(1), CorrectionLevel.L)[0];
+        for (int i = 0; i < correctionBytes.Length; ++i)
+        { Assert.Equal(expectedBytes[0][i], correctionBytes[i]); }
+    }
+}
---
+++ QuaRCSharp.Tests/MaskingTest.cs
@@ -0,0 +1,94 @@
+﻿using QuaRCSharp.Canvas;
+using QuaRCSharp.Canvas.Masking;
+
+namespace QuaRCSharp.Tests;
+
+public class MaskingTest
+{
+    // this is ugly
+    [Theory]
+    [InlineData(MaskNumber.Unmasked, new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False })] 
+    [InlineData(MaskNumber.Zero, 
+        new[]{ CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[]{ CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True },
+        new[]{ CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[]{ CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True },
+        new[]{ CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[]{ CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True }) ]
+    [InlineData(MaskNumber.One, 
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False }) ]
+    [InlineData(MaskNumber.Two, 
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False }) ]
+    [InlineData(MaskNumber.Three, 
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False }) ]
+    [InlineData(MaskNumber.Four, 
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False }) ]
+    [InlineData(MaskNumber.Five, 
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False }) ]
+    [InlineData(MaskNumber.Six, 
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True }) ]
+    [InlineData(MaskNumber.Seven, 
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True },
+        new[] { CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.True },
+        new[] { CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False, CanvasBitValue.False },
+        new[] { CanvasBitValue.False, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.True, CanvasBitValue.False, CanvasBitValue.False }) ]
+    public void MaskCanvas(MaskNumber maskNumber, params CanvasBitValue[][] expectedPattern)
+    {
+        CanvasBit[,] canvas = new CanvasBit[6,6];
+        for (int x = 0; x < 6; ++x)
+        {
+            for (int y = 0; y < 6; ++y)
+            { canvas[x, y] = new CanvasBit() { Value = CanvasBitValue.False, Position = (x, y)}; }
+        }
+
+        Mask mask = Masks.GetMaskByNumber(maskNumber);
+        for (int x = 0; x < 6; ++x)
+        {
+            for (int y = 0; y < 6; ++y)
+            { canvas[x, y] = mask.ApplyMaskToBit(canvas[x, y]); }
+        }
+        
+        for (int x = 0; x < 6; ++x)
+        {
+            for (int y = 0; y < 6; ++y)
+            { Assert.True(canvas[x, y].Value == expectedPattern[x][y]); }
+        }
+    }
+}
